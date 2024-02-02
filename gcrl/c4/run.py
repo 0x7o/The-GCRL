aa8a5039c6989f97b8298796b7a8a7fcaac115a0
0x7o <danil.kononyk@ya.ru>
2024-02-02 11:40:31 +0800

    Загрузка чанков на S3

--- gcrl/c4/run.py
+++ gcrl/c4/run.py
@@ -1,12 +1,19 @@
+import gzip
 import json
 
 from utils import calculate_entropy
 from chunks import C4Chunks
 from tqdm import tqdm
+import boto3
 import os
 
 c4 = C4Chunks()
 output_dir = "./c4-cleaned"
+session = boto3.session.Session(
+    aws_access_key_id=os.environ["S3_KEY"],
+    aws_secret_access_key=os.environ["S3_SECRET"],
+)
+s3 = session.client(service_name="s3", endpoint_url="https://storage.yandexcloud.net")
 
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
@@ -27,14 +34,27 @@ def main():
             if entropy < 4.6 or entropy >= 4.7:
                 continue
 
+            if len(text) < 512:
+                continue
+
             data["text"].append(text)
 
-        with open(
-                os.path.join(output_dir, f"c4-cleaned-{chunk_num:05}.json"),
-                "w",
-                encoding="utf-8",
+        file_name = os.path.join(output_dir, f"c4-cleaned-{chunk_num:05}.json.gz")
+
+        with gzip.open(
+            file_name,
+            "wb",
         ) as f:
-            json.dump(data, f, indent=4)
+            f.write(json.dumps(data).encode("utf-8"))
+
+        c4.remove_chunk(chunk_num)
+
+        with open(file_name, "rb") as f:
+            s3.upload_fileobj(
+                f, "0x7o", "c4-ru-cleaned/" + f"c4-cleaned-{chunk_num:05}.json.gz"
+            )
+
+        os.remove("c4-ru-cleaned/" + f"c4-cleaned-{chunk_num:05}.json.gz")
 
 
 if __name__ == "__main__":
